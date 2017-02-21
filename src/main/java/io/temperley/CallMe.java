17339e66cf2df092511cf3d37d8aaeccf0964a9c
will <will@MacBook-Pro-di-will.local>
2017-02-21 09:12:17 +0100

    init

---
+++ src/main/java/io/temperley/CallMe.java
@@ -0,0 +1,22 @@
+package io.temperley;
+
+/**
+ * Created by will on 20/02/2017.
+ */
+public class CallMe {
+
+    //Ideas:
+    //Add layer
+    //Add map objects
+    //Dynamic styles
+    //Script over countries etc (provide api)
+
+
+
+    public static String fun1(String name) {
+        System.out.format("Hi there from Java, %s", name);
+        System.getProperty("");
+        return "greetings from java";
+    }
+
+}
